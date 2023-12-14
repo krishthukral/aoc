3a0a5b08e8ecc82f58aa79a168adda214324c0f3
krishthukral <59071300+krishthukral@users.noreply.github.com>
2023-12-13 22:57:42 -0800

    hi

--- aoc5.js
+++ aoc5.js
@@ -1,8 +1,9 @@
-const dataStructure = [
-  [532, 98, 2],
-  [52, 50, 48]
-];
+const dataStructure = new Map([
+  [50, 98],
+  [52, 50]
+]);
 
 // Accessing values
-console.log(dataStructure[0][1]); // Output: 98
-console.log(dataStructure[1][2]); // Output: 48
+console.log(dataStructure.get(50)); // Output: 98
+console.log(dataStructure.get(52)); // Output: 50
+
